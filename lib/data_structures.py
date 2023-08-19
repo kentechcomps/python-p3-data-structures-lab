05de868b40ec6925dca9a2b9e15752e5f81da0b9
user <mutukukennedy34@gmail.com>
2023-08-19 12:36:17 +0300

    spiciestfood

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,4 +1,4 @@
-spicy_foods = [
+spiciest_foods = [
     {
         "name": "Green Curry",
         "cuisine": "Thai",
@@ -17,22 +17,41 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    names = [food["name"] for food in spicy_foods]
+    return names
+    
 
 def get_spiciest_foods(spicy_foods):
-    pass
+     spiciest_foods = [food for food in spicy_foods if food["heat_level"] > 5]
+     return spiciest_foods
+ 
 
-def print_spicy_foods(spicy_foods):
-    pass
+def print_spicy_foods(spiciest_foods):
+        for food in spiciest_foods:
+         heat_level_emojis = "🌶" * food["heat_level"]
+         print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level_emojis}")
 
-def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
 
-def print_spiciest_foods(spicy_foods):
-    pass
+def get_spicy_food_by_cuisine(spiciest_foods, cuisine):
+     for food in spiciest_foods:
+        if food["cuisine"] == cuisine:
+            return food
+     return None
 
-def get_average_heat_level(spicy_foods):
-    pass
+def print_spiciest_foods(spiciest_foods):
+      spiciest_foods = get_spiciest_foods(spiciest_foods)
+      for food in spiciest_foods:
+        heat_level_emojis = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level_emojis}")
 
-def create_spicy_food(spicy_foods, spicy_food):
-    pass
+
+def get_average_heat_level(spiciest_foods):
+     total_heat = sum(food["heat_level"] for food in spiciest_foods)
+     num_foods = len(spiciest_foods)
+    
+     if num_foods > 0:
+        average = total_heat / num_foods
+        return int(average)
+     else:
+        return 0
+
